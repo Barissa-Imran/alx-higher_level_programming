33e1001887911d9ee0a2665a7d4f9eabe4a9b60b
Barissa-Imran <imranbarissa@gmail.com>
2023-01-08 01:39:13 +0300

    Add 2-args.py
    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    def print_list(argument):
-        if argument is None:
-            print("0 arguments")
-        else:
-            argv = argument.split(" ")
-            length = len(argv)
+    """Print the number of and list of arguments."""
+    import sys # to access argv
 
-            if length == 1:
-                plural = "argument"
-            else:
-                plural = "arguments"
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
 
-            print(f'{length} {plural}:')
-
-            count = 0
-            for arg in argv:
-                print("{}: {}".format(count, arg))
-                count += 1
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
