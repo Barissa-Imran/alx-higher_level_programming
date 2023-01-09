653457b93241c7fc35f6b7ffef81147bfcb13671
Barissa-Imran <imranbarissa@gmail.com>
2023-01-09 17:11:58 +0300

    Add 4-new_in_list.py
    Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Replace an element in a list at a specific position without modifying the original list"""
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    else:
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
