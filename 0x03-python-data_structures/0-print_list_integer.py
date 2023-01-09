3b42f0f5915d36823aafeb60df9a942f800882f7
Barissa-Imran <imranbarissa@gmail.com>
2023-01-09 12:37:52 +0300

    Add 0-print_list_integer
    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Print items in a list"""
+
+
 def print_list_integer(my_list=[]):
     for i in my_list:
-        return i
+        print("{}".format(i))
 
-print("{}".format(print_list_integer(my_list)))
+
+print_list_integer(my_list)
