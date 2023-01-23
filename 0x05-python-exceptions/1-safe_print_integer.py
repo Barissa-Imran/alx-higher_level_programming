fab537770f091d46bb38d3423e9f78b64ca199ba
Barissa-Imran <imranbarissa@gmail.com>
2023-01-23 15:45:41 +0300

    Add 1-safe_print_integer
    Task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+def safe_print_integer(value):
+    """
+    Print an integer with "{:d}.format()
+    """
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
