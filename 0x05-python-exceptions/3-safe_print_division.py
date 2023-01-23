2c63ef26f4dd7654b99d31b5d78ab463b5035d04
Barissa-Imran <imranbarissa@gmail.com>
2023-01-23 22:19:48 +0300

    update 3-safe_print_division.py
    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         div = None
     finally:
-        print("Inside the: {}".format(div))
+        print("Inside result: {}".format(div))
     return (div)
