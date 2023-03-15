97a022931495d803a7a09456f5fbcc03fcfb4a4e
Imran <imranbarissa@gmail.com>
2023-03-15 22:35:53 +0300

    Task 7 - Add 7-cities.sql (fix errors)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
 	PRIMARY KEY(`id`),
-	`id` INT 	NOT NULL AUTO_INCREMENTED,
+	`id` INT 	NOT NULL AUTO_INCREMENT,
 	`state_id` INT	NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
 	FOREIGN KEY(`state_id`) REFERENCES `states`(`id`)
