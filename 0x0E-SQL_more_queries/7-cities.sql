4e433eacd046267cf01092a09b2f639dd0481581
Barissa-Imran <imranbarissa@gmail.com>
2023-03-15 14:25:20 +0300

    Task 7 - (fix error 2)

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
 	PRIMARY KEY(`id`),
-	`id` INT 	NOT NULL UNIQUE AUTO_INCREMENTED,
+	`id` INT 	NOT NULL AUTO_INCREMENTED,
 	`state_id` INT	NOT NULL,
 	`name` VARCHAR(256) NOT NULL,
-	FOREIGN KEY(`state_id`)
-	REFERENCES `hbtn_0d_usa`.`states`(`id`)
+	FOREIGN KEY(`state_id`) REFERENCES `states`(`id`)
 );
