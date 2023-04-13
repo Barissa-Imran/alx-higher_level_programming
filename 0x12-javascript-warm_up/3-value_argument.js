23d19b954a29c9321dc5e2e4116bf51a39cc83d5
Barissa-Imran <imranbarissa@gmail.com>
2023-04-13 23:06:55 +0300

    fix task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2])
+
+let myVar;
+if (typeof process.argv[2] === "undefined") {
+  myVar = "No argument";
+} else {
+  myVar = process.argv[2];
+}
+console.log(myVar);
