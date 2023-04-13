1cb303ff0afb794ffe859cbc1c577f8e2565a63b
Barissa-Imran <imranbarissa@gmail.com>
2023-04-14 00:43:16 +0300

    Add task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  } else {
+    return n * factorial (n - 1);
+  }
+}
+
+console.log(factorial(Number(process.argv[2])));
