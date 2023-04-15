547a15b69cbf17434dad2c8e987358922346214e
Barissa-Imran <imranbarissa@gmail.com>
2023-04-15 23:02:14 +0300

    Add ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,4 +13,3 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,4 +14,3 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-
