8c3afa66a98679de9551d4e9dfd113bfc93556e0
Barissa-Imran <imranbarissa@gmail.com>
2023-04-16 00:06:27 +0300

    Add task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+contains the class definition of a State and an
+instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+meta = MetaData()
+Base = declarative_base(metadata=meta)
+
+class State(Base):
+    """class repr state with is and name"""
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False,
+                primary_key=True)
+    name = Column(String(120), nullable=False)
