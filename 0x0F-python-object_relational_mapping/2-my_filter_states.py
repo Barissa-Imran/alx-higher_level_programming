e4d90ec7936c0258a273650068441bcc0913a851
Barissa-Imran <imranbarissa@gmail.com>
2023-04-15 23:21:27 +0300

    add 2-3 mod

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY '{}'""")
+                LIKE BINARY '{}'""".format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
