b4e2f58c60685c533c5dc990ad4a1da8bd270d26
Barissa-Imran <imranbarissa@gmail.com>
2023-04-27 23:46:18 +0300

    Add lines

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a peak-finding algorithm"""
 
+
 def find_peak(list_of_intergers):
     """Finds the peaks in a list of unsorted integers."""
     if list_of_intergers == []:
