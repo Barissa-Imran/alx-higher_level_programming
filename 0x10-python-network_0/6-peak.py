d56ed00faa778016c2f6895c7698e0bce04115db
Barissa-Imran <imranbarissa@gmail.com>
2023-04-28 00:08:19 +0300

    add some changes

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,25 +2,20 @@
 """Defines a peak-finding algorithm"""
 
 
-def find_peak(list_of_intergers):
+def find_peak(list_of_integers):
     """Finds the peaks in a list of unsorted integers."""
-    if list_of_intergers == []:
+    if list_of_integers == []:
         return None
 
-    length = len(list_of_intergers)
-    mid = int(length / 2)
-    li = list_of_intergers
+    length = len(list_of_integers)
+    left = 0
+    right = length - 1
 
-    if mid - 1 < 0 and mid + 1 >= length:
-        return li[mid]
-    elif mid - 1 < 0:
-        return li[mid] if li[mid] > li[mid + 1] else li[mid + 1]
-    elif mid + 1 >= length:
-        return li[mid] if li[mid] > li[mid - 1] else li[mid - 1]
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
 
-    if li[mid - 1] < li[mid] > li[mid + 1]:
-        return li[mid]
-
-    if li[mid + 1] > li[mid - 1]:
-        return find_peak(li[mid:])
-    return find_peak(li[:mid])
+    return list_of_integers[left]
