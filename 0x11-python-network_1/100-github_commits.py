7ea4ac01391ef811169829d995182d8d3cb7708a
Barissa-Imran <imranbarissa@gmail.com>
2023-04-28 23:51:38 +0300

    Add task advanced 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GITHUB repository.
+Usage: ./100-github_commits.py <repo name> <repo owner>
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")
+            ))
+    except IndexError:
+        pass
