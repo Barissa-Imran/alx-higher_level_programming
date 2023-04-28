9ef37a7a739ffcc0026539abc96ba51245b41f89
Barissa-Imran <imranbarissa@gmail.com>
2023-04-28 23:13:21 +0300

    Add task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
