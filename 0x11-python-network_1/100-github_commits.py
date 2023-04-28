24595cd12f1241e97e5cbf2395cd3bfc20d4e653
Barissa-Imran <imranbarissa@gmail.com>
2023-04-28 23:53:03 +0300

    Fix formatting

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,8 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2], sys.argv[1])
+    url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],
+                                                              sys.argv[1])
 
     r = requests.get(url)
     commits = r.json()
