35edbfcd9a82d456fe9e0a48cacf64e5965f4cd6
Barissa-Imran <imranbarissa@gmail.com>
2023-05-23 22:54:40 +0300

    Fix task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require('require');
+const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const results = JSON.parse(body).results;
